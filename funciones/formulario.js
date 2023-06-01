5f3566546811d19bbc79857080d40300d54b00f5
Sergio <71512161+SergJprog@users.noreply.github.com>
2023-06-01 11:56:07 -0600

    actualizacion formulario

--- funciones/formulario.js
+++ funciones/formulario.js
@@ -7,13 +7,13 @@ function selectRadio() {
     for (var i = 0; i < radios.length; i++) {
         if (radios[i].checked) {
 
-            if(radios[i].value === "telefono"){
+            if (radios[i].value === "telefono") {
                 let telefono = document.getElementById("tele").style.display = "block"
                 let mail = document.getElementById("mail").style.display = 'none'
-            }else if (radios[i].value === "Whatsapp"){
+            } else if (radios[i].value === "Whatsapp") {
                 let telefono = document.getElementById("tele").style.display = "block"
                 let mail = document.getElementById("mail").style.display = 'none'
-            }else {
+            } else {
                 let mail = document.getElementById("mail").style.display = "block"
                 let telefono = document.getElementById("tele").style.display = "none"
             }
@@ -22,5 +22,63 @@ function selectRadio() {
 
     }
 }
+
+let ubicaciones = ["Municipalidad", "EMETRA", "EMPAGUA"];
+let dependencias = ["Selecciona...","Atención al Vecino", "Catastro", "Desarrollo Social", "Informática", "Obras", "EMETRA", "EMPAGUA"];
+
+let selectUbicaciones = document.getElementById('select-Ubicacion');
+let selectDependencias = document.getElementById('select-Dependencia')
+let label_dependencia = document.getElementById('label-Depen')
+
+selectDependencias.style.display='NONE'
+label_dependencia.style.display='NONE'
+
+function recorrer(depen, valores) {
+    selectDependencias.innerHTML = '';
+
+    for (let index of valores) {
+        depen.innerHTML += `<option>${index}</option>`
+    }
+}
+
+function llenarUbicacion() {
+    recorrer(selectUbicaciones, ubicaciones)
+}
+
+
+llenarUbicacion()
+
+selectUbicaciones.addEventListener('change', (e) => {
+    let dato = e.target.value
+
+    switch (dato) {
+        case 'Municipalidad':
+            recorrer(selectDependencias, dependencias.slice(0, 5));
+            selectDependencias.style.display='BLOCK'
+            label_dependencia.style.display='BLOCK'
+            break;
+        case 'EMETRA':
+            recorrer(selectDependencias, dependencias.slice(6, 7));
+            label_dependencia.style.display='NONE'
+            selectDependencias.style.display='NONE'
+            break;
+        case 'EMPAGUA':
+            recorrer(selectDependencias, dependencias.slice(7, 8));
+            label_dependencia.style.display='NONE'
+            selectDependencias.style.display='NONE'
+            break;
+
+        default:
+            label_dependencia.style.display='NONE'
+            selectDependencias.style.display='NONE'
+            break;
+    }
+
+})
+
+
+
+
+
 
 
