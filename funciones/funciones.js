fe92bd16b2b8d3c8a25c183c2fefc1c23194139d
Sergio <71512161+SergJprog@users.noreply.github.com>
2023-06-29 09:26:28 -0600

    nueva actualizacion

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -16,7 +16,7 @@ function iniciar() {
   }
 
 
-  let completo = new manual('completo', 'manual.html', 'imagenes/PORTADA.jpg');
+  let completo = new manual('completo', 'manuales/manual.html', 'imagenes/PORTADA.jpg');
   let primera = new manual('primera', 'manual.html', 'imagenes/AMARILLO.jpg');
   let segunda = new manual('segunda', 'https://heyzine.com/flip-book/b925ceca84.html', 'imagenes/TURQUESA.jpg');
   let tercera = new manual('tercera', 'https://heyzine.com/flip-book/ce4546a8db.html', 'imagenes/MORADO1.jpg');
