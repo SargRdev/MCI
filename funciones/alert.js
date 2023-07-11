4e74fb89498acc46f8e55dda2818e3901cf5b98b
Sergio <71512161+SergJprog@users.noreply.github.com>
2023-07-11 11:02:33 -0600

    formulario validacion

--- funciones/alert.js
+++ funciones/alert.js
@@ -8,27 +8,28 @@ info.addEventListener('click', verificarFormulario)
 var campo1 = document.getElementById("nombre");
 var campo2 = document.getElementById("select-Ubicacion");
 var campo3 = document.getElementById("select-Dependencia");
+var campo4 = document.getElementById("area");
 
 
 console.log(campo3)
 function verificarFormulario() {
     // Obtener referencias a los campos del formulario
- 
 
     // Verificar si los campos están llenos
-    if (campo1.value === "" || campo2.value === "" || campo3.value === "") {
+    if (campo1.value === "" || campo2.value === "Selecciona..." || campo2.value === "" ||
+        campo3.value === "Selecciona..." || campo3.value === "" || campo4.value === "") {
 
         Swal.fire(
-            'Lo siento!',
-            'Llena todos los campos!',
+            'Oops...',
+            '¡Completa tus datos para contactarte!',
             'warning'
         )
         // Al menos un campo está vacío
         return false; // Evita el envío del formulario
     }
 
-    else{
-        
+    else {
+
         Swal.fire({
             title: '<p>En menos de lo que te imaginas, sabrás de nosotros...</p>',
             text: '',
@@ -37,10 +38,10 @@ function verificarFormulario() {
             imageHeight: 200,
             imageAlt: 'Custom image',
         })
-      
+  // Todos los campos están llenos
+  return true; // Permite el envío del formulario
     }
-   // Todos los campos están llenos
-   return true; // Permite el envío del formulario
+  
 }
 
 
